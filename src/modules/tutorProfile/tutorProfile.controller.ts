548989d1794c03118f8b3985e4f2cbf5f001bd9e
Rahmot15 <mdrahmatulla666@gmail.com>
2026-02-20 11:02:18 +0600

    add catchAsync for error handling

--- src/modules/tutorProfile/tutorProfile.controller.ts
+++ src/modules/tutorProfile/tutorProfile.controller.ts
@@ -1,24 +1,26 @@
 import { Request, Response } from "express";
 import { TutorProfileService } from "./tutorProfile.service";
+import catchAsync from "../../utils/catchAsync";
 
-const getAllTutors = async (req: Request, res: Response) => {
+const getAllTutors = catchAsync(async (req: Request, res: Response) => {
   const result = await TutorProfileService.getAllTutors();
 
-  res.json({
+  res.status(200).json({
     success: true,
     data: result,
   });
-};
+});
 
-const getSingleTutor = async (req: Request, res: Response) => {
+const getSingleTutor = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
+
   const result = await TutorProfileService.getSingleTutor(id as string);
 
-  res.json({
+  res.status(200).json({
     success: true,
     data: result,
   });
-};
+});
 
 export const TutorProfileController = {
   getAllTutors,
